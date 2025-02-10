98becdf1208d7fb0ddc61ed4ed1995dd4c1a0636
Ron <rfreidel@gmail.com>
2025-02-10 12:44:26 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -10,7 +10,7 @@ log_message() {
 log_message "Installing packages in the jail '$JAIL_NAME'..."
 sudo jexec "$JAIL_NAME" pkg update
 sudo jexec "$JAIL_NAME" pkg install -y wine wine64 wine-proton winetricks zenity
-sudo jexec "$JAIL_NAME" winetricks steam dxvk dxvk_nvapi"
+sudo jexec "$JAIL_NAME" winetricks dxvk dxvk_nvapi steam
 
 log_message "Verifying the installation of wine-proton winetricks steam..."
 if sudo jexec "$JAIL_NAME" pkg info | grep -q "wine-proton"; then
@@ -21,6 +21,6 @@ else
 fi
 
 log_message "Setting up environment variables and initializing Wine..."
-sudo jexec "$JAIL_NAME" sh -c "setenv WINE /usr/local/bin/wine; setenv WINE64 /usr/local/bin/wine64; setenv WINEARCH win64; wineboot --init"
+sudo jexec "$JAIL_NAME" sh -c "setenv WINE /usr/local/wine-proton/bin/wine; setenv WINE64 /usr/local/wine-proton/bin/wine64; setenv WINEARCH win64; wineboot --init"
 
 log_message "Installation and setup completed successfully."
