ba641ace96d3220fa79d09afee302f8065695fb4
Ron <rfreidel@gmail.com>
2025-02-12 00:21:52 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -8,12 +8,18 @@ log_message() {
 }
 
 log_message "Installing packages in the jail '$JAIL_NAME'..."
-sudo jexec "$JAIL_NAME" pkg update
-sudo jexec "$JAIL_NAME" pkg install -y wine wine64 wine-proton winetricks zenity
-sudo jexec "$JAIL_NAME" winetricks dxvk vkd3d nvapi vcrun2022 steam
+sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg update
+sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg install -y wine-proton winetricks zenity
+sudo /usr/local/sbin/jexec "$JAIL_NAME" WINE=/usr/local/wine-proton/bin/wine winetricks cmd dxvk vkd3d nvapi vcrun2022
+sudo fetch -o "$JAIL_NAME" /tmp/SteamSetup.exe https://cdn.fastly.steamstatic.com/client/installer/SteamSetup.exe
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games /usr/local/wine-proton/bin/wine /tmp/SteamSetup.exe
+
+# Patches to make games open in steam (Stolen from Suyimazu/Mizutamari)
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games WINE /usr/local/wine-proton/bin/wine $WINEBIN reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games WINE /usr/local/wine-proton/bin/wine $WINEBIN reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f
 
 log_message "Verifying the installation of wine-proton winetricks steam..."
-if sudo jexec "$JAIL_NAME" pkg info | grep -q "wine-proton"; then
+if sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg info | grep -q "wine-proton"; then
     log_message "wine-proton is installed successfully in the jail '$JAIL_NAME'."
 else
     log_message "Failed to install wine-proton in the jail '$JAIL_NAME'."
@@ -21,6 +27,6 @@ else
 fi
 
 log_message "Setting up environment variables and initializing Wine..."
-sudo jexec "$JAIL_NAME" sh -c "setenv WINE /usr/local/wine-proton/bin/wine; setenv WINE64 /usr/local/wine-proton/bin/wine; setenv WINEARCH win64; wineboot --init"
+sudo /usr/local/sbin/jexec "$JAIL_NAME" sh -c "setenv WINEPATH ~.steam-games WINE /usr/local/wine-proton/bin/wine; setenv WINE64 /usr/local/wine-proton/bin/wine; setenv WINEARCH win64; wineboot --init"
 
 log_message "Installation and setup completed successfully."
