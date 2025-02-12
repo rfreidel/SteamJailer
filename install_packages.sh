8fd4a46b8bcebae615f97dff047c6ea2472e3861
Ron <rfreidel@gmail.com>
2025-02-12 02:33:47 -0800

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -8,15 +8,21 @@ log_message() {
 }
 
 log_message "Installing packages in the jail '$JAIL_NAME'..."
-sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg update
-sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg install -y wine-proton winetricks zenity
-sudo /usr/local/sbin/jexec "$JAIL_NAME" WINE=/usr/local/wine-proton/bin/wine winetricks cmd dxvk vkd3d nvapi vcrun2022
-sudo fetch -o "$JAIL_NAME" /tmp/SteamSetup.exe https://cdn.fastly.steamstatic.com/client/installer/SteamSetup.exe
-/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games /usr/local/wine-proton/bin/wine /tmp/SteamSetup.exe
+sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg update || { log_message "Failed to update pkg repository." ; exit 1; }
+sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg install -y wine-proton winetricks zenity || { log_message "Failed to install required packages." ; exit 1; }
 
-# Patches to make games open in steam (Stolen from Suyimazu/Mizutamari)
-/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games WINE /usr/local/wine-proton/bin/wine $WINEBIN reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f
-/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games WINE /usr/local/wine-proton/bin/wine $WINEBIN reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f
+log_message "Running winetricks to install additional components..."
+sudo /usr/local/sbin/jexec "$JAIL_NAME" WINE=/usr/local/wine-proton/bin/wine winetricks cmd dxvk vkd3d nvapi vcrun2022 || { log_message "Failed to run winetricks." ; exit 1; }
+
+log_message "Downloading Steam setup..."
+sudo fetch -o /tmp/SteamSetup.exe https://cdn.fastly.steamstatic.com/client/installer/SteamSetup.exe || { log_message "Failed to download Steam setup." ; exit 1; }
+
+log_message "Installing Steam setup..."
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games /usr/local/wine-proton/bin/wine /tmp/SteamSetup.exe || { log_message "Failed to install Steam setup." ; exit 1; }
+
+log_message "Applying registry patches..."
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games /usr/local/wine-proton/bin/wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer" /t "REG_SZ" /d "" /f || { log_message "Failed to apply registry patch for gameoverlayrenderer." ; exit 1; }
+/usr/local/sbin/jexec "$JAIL_NAME" WINEPREFIX=~/.steam-games /usr/local/wine-proton/bin/wine reg.exe ADD "HKEY_CURRENT_USER\Software\Wine\DllOverrides" /v "gameoverlayrenderer64" /t "REG_SZ" /d "" /f || { log_message "Failed to apply registry patch for gameoverlayrenderer64." ; exit 1; }
 
 log_message "Verifying the installation of wine-proton winetricks steam..."
 if sudo /usr/local/sbin/jexec "$JAIL_NAME" pkg info | grep -q "wine-proton"; then
@@ -27,6 +33,6 @@ else
 fi
 
 log_message "Setting up environment variables and initializing Wine..."
-sudo /usr/local/sbin/jexec "$JAIL_NAME" sh -c "setenv WINEPATH ~.steam-games WINE /usr/local/wine-proton/bin/wine; setenv WINE64 /usr/local/wine-proton/bin/wine; setenv WINEARCH win64; wineboot --init"
+sudo /usr/local/sbin/jexec "$JAIL_NAME" sh -c "setenv WINEPATH ~/.steam-games WINE=/usr/local/wine-proton/bin/wine; setenv WINE64 /usr/local/wine-proton/bin/wine; setenv WINEARCH win64; wineboot --init" || { log_message "Failed to set up environment variables and initialize Wine." ; exit 1; }
 
 log_message "Installation and setup completed successfully."
