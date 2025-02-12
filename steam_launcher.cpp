00d707f93b7a098aa2b4a8cba16728ab80e14399
Ron <rfreidel@gmail.com>
2025-02-11 20:36:33 -0800

    Update steam_launcher.cpp

--- steam_launcher.cpp
+++ steam_launcher.cpp
@@ -40,19 +40,20 @@ std::string show_file_selection_dialog() {
 void set_wine_environment() {
     setenv("WINE", "/usr/local/wine-proton/bin/wine", 1);
     setenv("WINE-PROTON", "/usr/local/wine-proton/bin/wine", 1);
+    setenv{"WINE-PATH", "~/.steam-games", 1);
     setenv("WINEARCH", "win32", 1);
 }
 
 void install_wine_proton() {
     show_message("Installing Wine-Proton in Jail...");
     run_command("sudo jexec pkg install -y wine-proton winetricks");
-    run_command("/usr/local/wine-proton/bin/pkg32.sh install wine-proton mesa-dri
+    run_command("/usr/local/wine-proton/bin/pkg32.sh install wine-proton mesa-dri");
     
     show_message("Installation completed. Wine-Proton is installed in the jail.");
 }
 
 void install_dxvk() {
-    show_message("Installing winetricks in Jail...");
+    show_message("Installing those gol darn thangs in Jail...");
     run_command("jexec WINE=/usr/local/wine-proton/bin/wine winetricks --force corefonts dxvk nvapi steam");
     show_message("dxvk & steam installation completed successfully.");
 }
