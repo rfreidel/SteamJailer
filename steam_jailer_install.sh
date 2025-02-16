88a7782211c7718ac094c21595f70e726699ea0c
rfreidel <rfreidel@gmail.com>
2025-02-15 16:10:31 -0800

    Update steam_jailer_install.sh

--- steam_jailer_install.sh
+++ steam_jailer_install.sh
@@ -7,7 +7,6 @@ echo "Starting Steam Jailer installation..."
 # Step 1: Set up the Steam Jailer
 c++ steamjailer_installer.cpp -o jailer
 
-
 # Step 2: Install Wine-Proton and Winetricks inside the jail
 echo "Running Steam setup..."
 
